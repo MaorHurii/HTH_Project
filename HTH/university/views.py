5041be0f9d37252cc2ab6cbf36636e28cfccea8a
Maor <maorhu@ac.sce.ac.il>
2023-01-08 14:35:35 +0200

    wip

--- HTH/university/views.py
+++ HTH/university/views.py
@@ -1,27 +1,69 @@
+from django.contrib.auth import authenticate, login as login_user, logout as logout_user
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseForbidden
 from django.contrib import messages
+from .models import Course, Appointment, Report, Question, Answer, File
 from .forms import CourseForm, AppointmentForm, AnswerForm
-from .models import Course, Appointment, Report, Question, Answer
+
+
+ADMIN_ROLE = 'Admin'
+TEACHER_ROLE = 'Teacher'
+STUDENT_ROLE = 'Student'
+TEACHER_ADMIN_ROLE = 'student_admin'
+STUDENT_TEACHER_ROLE = 'student_teacher'
 
 
 def index(request):
     return render(request, 'university/index.html')
 
 
-def admin_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('admin_home')
-        else:
-            messages.warning(request, 'Invalid username or password')
-            return redirect('admin_login')
-    return render(request, 'university/admin_login.html')
+def login(request, role):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None and validate_role(user, role):
+        login_user(request, user)
+        return True
+    else:
+        messages.warning(request, 'Invalid username or password')
+        return False
+
+
+def validate_role(user, role):
+    if role == ADMIN_ROLE:
+        return user.is_superuser
+    elif role == TEACHER_ADMIN_ROLE:
+        return user.is_superuser or user.groups.filter(name=TEACHER_ROLE).exists()
+    elif not user.is_superuser and role != STUDENT_TEACHER_ROLE:
+        return user.groups.filter(name=role).exists()
+    elif role == STUDENT_TEACHER_ROLE:
+        return user.groups.filter(name__in=[TEACHER_ROLE, STUDENT_ROLE]).exists()
+    return False
+
+
+def role_required(role):
+    def decorator(func):
+        def wrapper(request, *args, **kwargs):
+            # Check if user is authenticated
+            if not request.user.is_authenticated:
+                return login_required(func)(request, *args, **kwargs)
+            # check if user has the required role
+            if not validate_role(request.user, role):
+                return HttpResponseForbidden('You do not have the required role to access this page')
+            return func(request, *args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@login_required
+def logout(request):
+    logout_user(request)
+    return redirect('index')
+
+# Admin context start
+
 
 def admin_login(request):
     if request.method == 'POST':
@@ -90,6 +132,10 @@ def delete_report(request, report_id):
     report = Report.objects.get(id=report_id)
     report.delete()
     return redirect('admin_home')
+
+# Admin context end
+
+# Teacher context start
 
 
 def teacher_login(request):
@@ -176,43 +222,34 @@ def answer_question(request, question_id):
         form = AnswerForm()
     return render(request, 'university/answer_question.html', {'form': form})
 
+# Teacher context end
+
+# Student context start
+
+
 def student_login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
+        if login(request, STUDENT_ROLE):
             return redirect('student_home')
         else:
-            messages.warning(request, 'Invalid username or password')
             return redirect('student_login')
     return render(request, 'university/student_login.html')
 
 
-@login_required(login_url='student_login')
-def student_logout(request):
-    logout(request)
-    return redirect('index')
-
-
+@role_required(STUDENT_ROLE)
 def student_home(request):
-    return render(request, 'university/student_home.html')
-
-
-@login_required(login_url='student_login')
-def view_courses(request):
     courses = Course.objects.all()
-    return render(request, 'university/view_courses.html', {'courses': courses})
+    files = File.objects.filter(teacher_file=False)
+    return render(request, 'university/student_home.html', {'courses': courses, 'files': files})
 
 
-@login_required(login_url='student_login')
+@role_required(STUDENT_ROLE)
 def view_questions(request):
     questions = Question.objects.all()
     return render(request, 'university/view_questions.html', {'questions': questions})
 
 
-@login_required(login_url='student_login')
+@role_required(STUDENT_ROLE)
 def create_question(request):
     if request.method == 'POST':
         title = request.POST['title']
@@ -221,12 +258,34 @@ def create_question(request):
         return redirect('view_questions')
     return render(request, 'university/create_question.html')
 
+# Student context end
 
-@login_required(login_url='student_login')
+
+# All context start
+
+@role_required(STUDENT_TEACHER_ROLE)
 def upload_file(request):
     if request.method == 'POST':
         file = request.FILES['file']
         filename = file.name
-        Report.objects.create(filename=filename, file=file, uploader=request.user.username)
-        return redirect('student_home')
+        teacher_file = validate_role(request.user, TEACHER_ROLE)
+        File.objects.create(filename=filename, file=file, uploader=request.user.username, teacher_file=teacher_file)
+        return redirect('teacher_home' if teacher_file else 'student_home')
     return render(request, 'university/upload_file.html')
+
+
+@login_required
+def delete_file(request, file_id):
+    file = File.objects.get(id=file_id)
+    role = TEACHER_ROLE if file.teacher_file else STUDENT_ROLE
+    if request.user.is_superuser or validate_role(request.user, role):
+        file.delete()
+    if request.user.is_superuser:
+        home = 'admin_home'
+    elif validate_role(request.user, TEACHER_ROLE):
+        home = 'teacher_home'
+    else:
+        home = 'student_home'
+    return redirect(home)
+
+# All context end
