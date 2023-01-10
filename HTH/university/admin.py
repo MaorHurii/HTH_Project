e01aa8392ea6e03ce5d5da4ce053b4e9084ec66e
Maor <maorhu@ac.sce.ac.il>
2023-01-10 18:04:43 +0200

    wip

--- HTH/university/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Course, Report, Appointment, Question, Answer
-
-admin.site.register(Course)
-admin.site.register(Report)
-admin.site.register(Appointment)
-admin.site.register(Question)
-admin.site.register(Answer)
